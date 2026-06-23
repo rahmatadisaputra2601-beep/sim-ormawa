9e34c08aa80650888030295a00e87e7dcb764d52
rahmatadisaputra2601-beep <rahmatadisaputra2601@gmail.com>
2026-06-23 12:21:46 +0700

    feat: final boss SIAKAD UI upgrade (sidebar, toast, animations, dashboard system)

---
+++ script.js
@@ -0,0 +1,96 @@
+function masukLogin() {
+    document.getElementById("landing").style.display = "none";
+    document.getElementById("loginBox").style.display = "block";
+}
+
+function login() {
+    let pass = document.getElementById("password").value;
+
+    if (pass === "1234") {
+        document.getElementById("loginBox").style.display = "none";
+        document.getElementById("app").style.display = "flex";
+        document.getElementById("landing").style.display = "none";
+        document.getElementById("error").innerText = "";
+    } else {
+        document.getElementById("error").innerText = "Password salah bro";
+    }
+}
+
+function logout() {
+    document.getElementById("app").style.display = "none";
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("landing").style.display = "block";
+}
+
+/* SECTION SWITCH */
+function showSection(id, event) {
+
+    document.querySelectorAll(".section").forEach(sec => {
+        sec.style.display = "none";
+    });
+
+    document.getElementById(id).style.display = "block";
+
+    document.querySelectorAll(".menu").forEach(m => {
+        m.classList.remove("active");
+    });
+
+    if (event) {
+        event.currentTarget.classList.add("active");
+    }
+}
+
+/* DATA STORAGE */
+function getData() {
+    return JSON.parse(localStorage.getItem("anggota")) || [];
+}
+
+function saveData(data) {
+    localStorage.setItem("anggota", JSON.stringify(data));
+}
+
+function loadData() {
+    let list = document.getElementById("list");
+    if (!list) return;
+
+    let data = getData();
+    list.innerHTML = "";
+
+    data.forEach((nama, i) => {
+        let li = document.createElement("li");
+        li.textContent = (i + 1) + ". " + nama;
+
+        let btn = document.createElement("button");
+        btn.textContent = "Hapus";
+        btn.onclick = () => hapus(i);
+
+        li.appendChild(btn);
+        list.appendChild(li);
+    });
+
+    let total = document.getElementById("total");
+    if (total) total.innerText = data.length;
+}
+
+function tambah() {
+    let nama = document.getElementById("nama").value;
+    if (!nama) return;
+
+    let data = getData();
+    data.push(nama);
+
+    saveData(data);
+    document.getElementById("nama").value = "";
+
+    loadData();
+}
+
+function hapus(i) {
+    let data = getData();
+    data.splice(i, 1);
+
+    saveData(data);
+    loadData();
+}
+
+window.onload = loadData;
